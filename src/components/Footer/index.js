8e4e0b9497b1b52f0b96c2dd1b52c9957bffa7b2
aryaaminata093 <arya_aminata93@apps.ipb.ac.id>
2021-11-22 14:11:04 +0700

    add footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,76 @@
+import { Box, Container, Grid, Link, makeStyles} from '@material-ui/core';
+
+const styles = makeStyles(() =>({
+    box:{
+        backgroundColor: "#1D1C21",
+    },
+    gridContainer:{
+        padding: "10px 0px",
+    },
+    title: {
+        textAlign: "center",
+        fontWeight: "bolder",
+        fontSize: "20px",
+        color: "white",
+    },
+    desc: {
+        textAlign: "center",
+        fontWeight: "normal",
+        fontSize: "16px",
+        color: "white",
+        cursor: "pointer",
+    },
+    link: {
+        color: "white",
+        textDecorationLine: "none",
+        '&:hover': {
+            color: '#644EEC',
+            textDecorationLine: "none",
+        }
+    }
+    // instagram: {
+    //     textAlign: "right",
+    // },
+    // twitter: {
+    //     textAlign: "right",
+    // },
+    // email: {
+    //     textAlign: "right",
+    // },
+}));
+
+export default function Footer(){
+    const classes = styles()
+
+    return(
+        <Box className={classes.box}>
+            <Container maxWidth='lg'>
+                <Grid container spacing={4} className={classes.gridContainer}>
+                    <Grid item xs={12} className={classes.title}>
+                        <Box >Mamen Project</Box>
+                    </Grid>
+                    <Grid item xs={1}></Grid>
+                    <Grid item xs={3} className={classes.desc} >
+                        <Box>
+                            Instagram :   
+                            <Link href="https://www.instagram.com/" className={classes.link}> @mamenrider</Link>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={4} className={classes.desc}>
+                        <Box>
+                            Twitter :   
+                            <Link href="https://twitter.com/" className={classes.link}> @mamenrider</Link>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={3} className={classes.desc}>
+                        <Box>
+                            Email :   
+                            <Link href="mailto:MamenRider@gmail.com" className={classes.link}> MamenRider@gmail.com</Link>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={1}></Grid>
+                </Grid>
+            </Container>
+        </Box>
+    );
+}
